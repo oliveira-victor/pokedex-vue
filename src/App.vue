00d48f0f8563255c7f621acbc88d93dfe310d719
oliveira-victor <victor.freire.oliveira@gmail.com>
2024-09-27 15:00:17 -0300

    feat: adding header

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,30 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="wrapper">
+    <div class="container">
+      <Header />
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.wrapper {
+  max-width: 1920px;
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 10px;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.container {
+  max-width: 1024px;
+  width: 100%;
+  border-radius: 30px;
+  background-color: #fff;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <header class="header">
+        <h1>Pokedex</h1>
+        <form class="form">
+            <input type="text" placeholder="Pesquisar" required>
+            <button class="submit-btn" type="submit"><img class="search-icon" src="../assets/pokeball.webp"
+                    alt="Pokeball icon"></button>
+        </form>
+    </header>
+</template>
+
+<style>
+.header {
+    background-color: #ac1c1c;
+    background-image: url(../assets/waves.webp);
+    background-size: 120px;
+    display: flex;
+    justify-content: space-between;
+    gap: 20px;
+    padding: 20px 40px;
+    border-radius: 30px 30px 0 0;
+}
+
+.form {
+    max-width: 240px;
+    width: 100%;
+    position: relative;
+}
+
+input {
+    width: 100%;
+    height: 40px;
+    border: none;
+    border-radius: 20px;
+    padding: 5px 10px;
+    font-size: 16px;
+}
+
+.submit-btn {
+    position: absolute;
+    height: 40px;
+    top: 0;
+    right: 0;
+    cursor: pointer;
+    border: none;
+    background: none;
+}
+
+.search-icon {
+    height: 40px;
+    transition: all .2s ease;
+}
+
+.search-icon:hover {
+    transform: scale(1.07) rotate(15deg);
+}
+</style>
