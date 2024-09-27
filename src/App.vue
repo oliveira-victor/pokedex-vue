2392ddf468447b4b89622b09232cb026269faef3
oliveira-victor <victor.freire.oliveira@gmail.com>
2024-09-27 15:11:47 -0300

    feat: adding footer

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 import Header from './components/Header.vue'
 import MainContent from './components/MainContent.vue';
+import Footer from './components/Footer.vue';
 </script>
 
 <template>
   <div class="wrapper">
     <div class="container">
       <Header />
-      <MainContent />?
+      <MainContent />
+      <Footer />
     </div>
   </div>
 </template>
---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <footer class="footer">
+        Desenvolvido por Victor F. Oliveira
+    </footer>
+</template>
+
+<style>
+.footer {
+    color: #fff;
+    background-color: #ac1c1c;
+    background-image: url(../assets/waves.webp);
+    background-size: 120px;
+    border-radius: 0 0 30px 30px;
+    padding: 10px 40px;
+    font-size: 12px;
+}
+</style>
